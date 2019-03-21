53caeaf17b6b9d85a79f93a9f19a469d837fa57b
David Salgado <david@digitalronin.com>
2019-03-21 19:22:41 +0000

    Add initial state and bill total input field

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
 class Main extends Component {
+  state = {
+    total: '',
+    numPeople: 1
+  }
+
+  handleTotalChange(event) {
+    const total = event.target.value.replace(/[^.\d]/g, '')
+    this.setState({ total });
+  }
+
   render() {
     return (
-      <h1>Main</h1>
+      <div>
+        <h1>Bill splitter</h1>
+        <div>
+          <label>Total amount</label>
+          <input
+            value={this.state.total}
+            onChange={(event) => this.handleTotalChange(event)}
+          />
+        </div>
+      </div>
     );
   }
 }
