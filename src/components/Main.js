e57bfc6d03ce080899b79191142801e374aa1799
David Salgado <david@digitalronin.com>
2019-03-21 20:19:22 +0000

    Track contributions
    This version simply divides the total by the
    number of payers. Next step is to enable fixing
    a contribution.

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,17 +4,49 @@ import NumberInput from './NumberInput';
 class Main extends Component {
   state = {
     total: '',
-    numPeople: 1
+    numPeople: 1,
+    contributions: {}
   }
 
   handleTotalChange(event) {
     const total = event.target.value.replace(/[^.\d]/g, '')
+    this.calculateContributions(total, this.state.numPeople);
     this.setState({ total });
   }
 
   handleNumPeopleChange(event) {
-    const numPeople = event.target.value.replace(/\D/g, '')
+    const numPeople = event.target.value.replace(/\D/g, '');
+    this.calculateContributions(this.state.total, numPeople);
     this.setState({ numPeople });
+  }
+
+  calculateContributions(total, numPeople) {
+    const tot = parseFloat(total);
+    const people = parseInt(numPeople);
+    if (tot && people) {
+      const contribution = tot / people;
+      let contributions = {};
+      Array(people).fill().map((_, i) => contributions[i] = contribution)
+      this.setState({ contributions });
+    }
+  }
+
+  payers() {
+    const numPeople = parseInt(this.state.numPeople);
+    if (numPeople) {
+      console.log(this.state.contributions);
+      return Object.keys(this.state.contributions).map((key, i) => {
+        const val = this.state.contributions[key];
+        const value = isNaN(val) ? '' : val;
+        return <NumberInput
+          label={`Payer ${i+1}`}
+          value={value}
+          key={key}
+        />
+      });
+    } else {
+      return null
+    }
   }
 
   render() {
@@ -31,6 +63,7 @@ class Main extends Component {
           value={this.state.numPeople}
           onChange={this.handleNumPeopleChange.bind(this)}
         />
+        {this.payers()}
       </div>
     );
   }
